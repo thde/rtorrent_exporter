d3ffc39e2976729769865ed662cd4ba2d5b05373
thde <thde@users.noreply.github.com>
2023-12-13 11:13:26 +0100

    feat(cli): support env variables

--- README.md
+++ README.md
@@ -14,18 +14,27 @@ A Docker container (`ghcr.io/thde/rtorrent_exporter:latest`) is also available t
 ```
 usage: rtorrent_exporter [<flags>]
 
+Prometheus exporter for rTorrent.
+
 Flags:
-  -h, --help                 Show context-sensitive help (also try --help-long and --help-man).
-      --web.config.file=""   [EXPERIMENTAL] Path to configuration file that can enable TLS or authentication.
-      --web.listen-address=":9135"
-                             Address to listen on for web interface and telemetry.
-      --web.telemetry-path="/metrics"
-                             Path under which to expose metrics.
-      --rtorrent.scrape-uri="http://localhost/RPC2"
-                             URI on which to scrape rTorrent. Use http://user:pass@host.com to supply basic auth credentials.
-      --rtorrent.ssl-verify  Flag that enables SSL certificate verification for the scrape URI.
-      --rtorrent.timeout=5s  Timeout for trying to get stats from rtorrent.
+  -h, --[no-]help            Show context-sensitive help (also try --help-long and --help-man). ($RTORRENT_EXPORTER_HELP)
+      --web.listen-address=:9135 ...  
+                             Addresses on which to expose metrics and web interface. Repeatable for multiple addresses.
+                             ($RTORRENT_EXPORTER_WEB_LISTEN_ADDRESS)
+      --web.config.file=""   Path to configuration file that can enable TLS or authentication. See:
+                             https://github.com/prometheus/exporter-toolkit/blob/master/docs/web-configuration.md
+                             ($RTORRENT_EXPORTER_WEB_CONFIG_FILE)
+      --web.telemetry-path="/metrics"  
+                             Path under which to expose metrics. ($RTORRENT_EXPORTER_WEB_TELEMETRY_PATH)
+      --rtorrent.scrape-uri="http://localhost/RPC2"  
+                             URI on which to scrape rTorrent. Use http://user:pass@host.com to supply basic auth
+                             credentials. ($RTORRENT_EXPORTER_RTORRENT_SCRAPE_URI)
+      --[no-]rtorrent.ssl-verify  
+                             Flag that enables SSL certificate verification for the scrape URI.
+                             ($RTORRENT_EXPORTER_RTORRENT_SSL_VERIFY)
+      --rtorrent.timeout=5s  Timeout for trying to get stats from rtorrent. ($RTORRENT_EXPORTER_RTORRENT_TIMEOUT)
       --log.level=info       Only log messages with the given severity or above. One of: [debug, info, warn, error]
-      --log.format=logfmt    Output format of log messages. One of: [logfmt, json]
-      --version              Show application version.
+                             ($RTORRENT_EXPORTER_LOG_LEVEL)
+      --log.format=logfmt    Output format of log messages. One of: [logfmt, json] ($RTORRENT_EXPORTER_LOG_FORMAT)
+      --[no-]version         Show application version. ($RTORRENT_EXPORTER_VERSION)
 ```
--- main.go
+++ main.go
@@ -5,44 +5,42 @@ import (
 	"os"
 
 	"github.com/alecthomas/kingpin/v2"
+	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	"github.com/mrobinsn/go-rtorrent/rtorrent"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/promlog"
+	logflag "github.com/prometheus/common/promlog/flag"
 	"github.com/prometheus/common/version"
 	"github.com/prometheus/exporter-toolkit/web"
-	"github.com/thde/rtorrent_exporter/exporter"
-
-	logflag "github.com/prometheus/common/promlog/flag"
 	webflag "github.com/prometheus/exporter-toolkit/web/kingpinflag"
+
+	"github.com/thde/rtorrent_exporter/exporter"
 )
 
-const namespace = "rtorrent"
+const (
+	namespace = "rtorrent"
+	name      = namespace + "_exporter"
+)
 
 var (
-	webConfig         = webflag.AddFlags(kingpin.CommandLine, ":9135")
-	metricsPath       = kingpin.Flag("web.telemetry-path", "Path under which to expose metrics.").Default("/metrics").String()
-	rtorrentScrapeURI = kingpin.Flag("rtorrent.scrape-uri", "URI on which to scrape rTorrent. Use http://user:pass@host.com to supply basic auth credentials.").Default("http://localhost/RPC2").String()
-	rtorrentSSLVerify = kingpin.Flag("rtorrent.ssl-verify", "Flag that enables SSL certificate verification for the scrape URI.").Default("true").Bool()
-	rtorrentTimeout   = kingpin.Flag("rtorrent.timeout", "Timeout for trying to get stats from rtorrent.").Default("5s").Duration()
+	cli               = kingpin.New(name, "Prometheus exporter for rTorrent.").DefaultEnvars()
+	webConfig         = webflag.AddFlags(cli, ":9135")
+	metricsPath       = cli.Flag("web.telemetry-path", "Path under which to expose metrics.").Default("/metrics").String()
+	rtorrentScrapeURI = cli.Flag("rtorrent.scrape-uri", "URI on which to scrape rTorrent. Use http://user:pass@host.com to supply basic auth credentials.").Default("http://localhost/RPC2").String()
+	rtorrentSSLVerify = cli.Flag("rtorrent.ssl-verify", "Flag that enables SSL certificate verification for the scrape URI.").Default("true").Bool()
+	rtorrentTimeout   = cli.Flag("rtorrent.timeout", "Timeout for trying to get stats from rtorrent.").Default("5s").Duration()
 )
 
-func main() {
-	promlogConfig := &promlog.Config{}
-	logflag.AddFlags(kingpin.CommandLine, promlogConfig)
-	kingpin.Version(version.Print("rtorrent_exporter"))
-	kingpin.HelpFlag.Short('h')
-	kingpin.Parse()
-	logger := promlog.New(promlogConfig)
-
+func run(logger log.Logger) error {
 	level.Info(logger).Log("msg", "Starting rtorrent_exporter", "version", version.Info())
 	level.Info(logger).Log("build_context", version.BuildContext())
 
-	prometheus.MustRegister(version.NewCollector("rtorrent_exporter"))
+	prometheus.MustRegister(version.NewCollector(name))
 
 	http.Handle(*metricsPath, promhttp.Handler())
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
 		_, _ = w.Write([]byte(`<!doctype html>
 		<html lang=en>
 		<head>
@@ -74,8 +72,23 @@ func main() {
 	prometheus.MustRegister(&e)
 
 	srv := &http.Server{}
-	if err := web.ListenAndServe(srv, webConfig, logger); err != nil {
-		level.Error(logger).Log("msg", "Error starting HTTP server", "err", err)
+	return web.ListenAndServe(srv, webConfig, logger)
+}
+
+func main() {
+	promlogConfig := &promlog.Config{}
+	logger := promlog.New(promlogConfig)
+
+	logflag.AddFlags(cli, promlogConfig)
+	cli.Version(version.Print(name))
+	cli.HelpFlag.Short('h')
+	if _, err := cli.Parse(os.Args[1:]); err != nil {
+		level.Error(logger).Log("msg", "Error parsing CLI flags", "err", err)
+		os.Exit(1)
+	}
+
+	if err := run(logger); err != nil {
+		level.Error(logger).Log("msg", "Error starting exporter", "err", err)
 		os.Exit(1)
 	}
 }
